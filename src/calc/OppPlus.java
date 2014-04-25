94b58abef5fa03f85de1648e38a289d43883f25c
CedricHanebaum <Cedric.Hanebaum@rwth-aachen.de>
2014-04-25 15:11:46 +0200

    fixed Bug. Wrong inputs with to many operators dont crash the program anymore.

--- src/calc/OppPlus.java
+++ src/calc/OppPlus.java
@@ -6,8 +6,12 @@ public class OppPlus extends Opperator{
 
 	@Override
 	public Value evaluate(Stack<Element> stack) {
-		double value = stack.pop().evaluate(stack).getValue() + stack.pop().evaluate(stack).getValue();
-		return new Value(value);
+		double[] vals = new double[2];
+		for(int i = 0; i < vals.length; ++i){
+			if(stack.isEmpty()) return new Value(Double.NaN);
+			vals[i] = stack.pop().evaluate(stack).getValue();
+		}
+		return new Value(vals[0] + vals[1]);
 	}
 	
 }
--- src/calc/OppPow.java
+++ src/calc/OppPow.java
@@ -6,8 +6,12 @@ public class OppPow extends Opperator{
 
 	@Override
 	public Value evaluate(Stack<Element> stack) {
-		double value = Math.pow(stack.pop().evaluate(stack).getValue(), stack.pop().evaluate(stack).getValue());
-		return new Value(value);
+		double[] vals = new double[2];
+		for(int i = 0; i < vals.length; ++i){
+			if(stack.isEmpty()) return new Value(Double.NaN);
+			vals[i] = stack.pop().evaluate(stack).getValue();
+		}
+		return new Value(Math.pow(vals[0], vals[1]));
 	}
 
 }
